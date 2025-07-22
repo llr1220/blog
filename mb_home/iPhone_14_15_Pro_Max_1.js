9d3f587775cf7ce38419153dfe6a8f595146b823
llr <1208448620@qq.com>
2025-07-22 23:10:29 +0800

    pc首页

---
+++ mb_home/iPhone_14_15_Pro_Max_1.js
@@ -0,0 +1,17 @@
+// 动态为根元素设置字体大小
+function init () {
+
+
+    // 获取屏幕宽度
+    var width = document.documentElement.clientWidth
+    // 设置根元素字体大小。此时为宽的10等分
+    document.documentElement.style.fontSize = width / 28 + 'px'
+}
+
+// 首次加载应用，设置一次
+init()
+// 监听手机旋转的事件的时机，重新设置
+window.addEventListener('orientationchange', init)
+// 监听手机窗口变化，重新设置
+window.addEventListener('resize', init)
+
